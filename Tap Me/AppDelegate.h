48bcde05380e60d21c87fb443e54e2e2e045b1bb
Rishabh Jain <rishabhjain2795@gmail.com>
2015-06-02 13:22:36 -0700

    Initial Commit

---
+++ Tap Me/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tap Me
+//
+//  Created by Rishabh Jain on 6/2/15.
+//  Copyright (c) 2015 Rishabh Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tap Me/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tap Me
+//
+//  Created by Rishabh Jain on 6/2/15.
+//  Copyright (c) 2015 Rishabh Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
